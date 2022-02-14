f3d83a7a2cc1972650248ac94ba258e4184c8003
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-02-14 17:43:23 +0300

    Реализация вывода заданных точек

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-import numpy
 import matplotlib.pyplot as plt
+import numpy as np
 import wx
 
-fig = plt.figure() # Создание объекта
-print (fig.axes) # Список текущих объектов
-print (type(fig)) # тип объекта figure
-
-plt.scatter(1.0 , 1.0) # scatter - метод для нанесения маркера в точке (1, 1)
-
-# После нанесения графического элемента в виде маркера
-# список текущих областей состоит из одной области
-
-print (fig.axes)
 
 
-plt.show()
+def show(matrix):
+    for i in range(matrix.shape[0]):
+        plt.scatter(matrix[i,0] , matrix[i,1]) # scatter - метод для нанесения маркера в точке
+
+    plt.show()
+
+# зависимость линейной функции y = a*x + b
+def lin_func(matrix):
+    return 0
+
+
+def main():
+    # Таблица значений некоторой функциональной зависимости
+    matrix = np.array([[1, 1], [2, 1.5], [3, 3], [4, 4.5], [5, 7], [6, 8.5]])
+
+    show(matrix) # Вывод точек
+
+    #print(matrix.shape[0])
+
+if __name__ == '__main__':
+    main()
