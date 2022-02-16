560e2b90b7a5a1739ce35ae51e9817f152c432f0
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-02-16 23:55:36 +0300

    Показательная функция

--- main.py
+++ main.py
@@ -77,6 +77,41 @@ def sed_func(matrix):
     result[0] = math.exp(result[0]).__round__(2)
     print(result)
 
+# Зависимость y от x в виде показательной функции y = B * e^(ax)
+def exp_func(matrix):
+    # x и Y = ln(y) получаем зависимость
+    matrix_ln = matrix.copy()  # Создаем копию массива
+    for i in matrix_ln:
+        i[1] = math.log(i[1])
+
+    # сумма по xi
+    sum_xi = 0
+    for i in range(matrix_ln.shape[0]):
+        sum_xi += matrix_ln[i, 0]
+
+    # сумма по yi
+    sum_yi = 0
+    for i in range(matrix_ln.shape[0]):
+        sum_yi += matrix_ln[i, 1]
+
+    # сумма по xi^2
+    sum_xi2 = 0
+    for i in range(matrix_ln.shape[0]):
+        sum_xi2 += pow(matrix_ln[i, 0], 2)
+
+    # сумма по xi*yi
+    sum_xi_yi = 0
+    for i in range(matrix_ln.shape[0]):
+        sum_xi_yi += matrix_ln[i, 0] * matrix_ln[i, 1]
+
+    # Найдем коэффициенты
+    m1 = np.array([[sum_xi2, sum_xi], [sum_xi, matrix_ln.shape[0]]])  # Матрица коэффициентов
+    v1 = np.array([sum_xi_yi, sum_yi])  # Вектор свободных членов
+    result = np.around(np.linalg.solve(m1, v1), 2)[::-1]
+
+    result[0] = math.exp(result[0]).__round__(2)
+    print(result)
+
 def main():
     # Таблица значений некоторой функциональной зависимости
     matrix = np.array([[1, 1], [2, 1.5], [3, 3], [4, 4.5], [5, 7], [6, 8.5]])
@@ -85,7 +120,8 @@ def main():
 
     #print(matrix.shape[0])
     #lin_func(matrix)
-    sed_func(matrix)
+    #sed_func(matrix)
+    exp_func(matrix)
 
 if __name__ == '__main__':
     main()
